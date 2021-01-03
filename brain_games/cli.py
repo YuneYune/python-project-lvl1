2b2f18a9c16b3735b33cdc27e849e00c986d195b
Dobro <matematikaigor@gmail.com>
2021-01-03 14:13:40 +0300

    refactor code to fix linter problems

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+"""Cli commands."""
+
+
 import prompt
 
 
 def welcome_user():
+    """Greeting."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+"""Main app."""
+
 from brain_games import cli
 
 
 def main():
+    """Call functions."""
     print('Welcome to the Brain Games!')
     cli.welcome_user()
 
