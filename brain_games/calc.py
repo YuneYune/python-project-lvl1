3237bbc4e60412d2430cadd2adc7d631ee0257ec
Dobro <matematikaigor@gmail.com>
2021-01-07 19:29:10 +0300

    change structure of functions

--- brain_games/calc.py
+++ brain_games/calc.py
@@ -11,11 +11,6 @@ crypto = random.SystemRandom()
 operators_as_str = ['+', '-', '*']
 
 
-def define_rules():
-    """Rules of the game."""
-    print('What is the result of the expression?')
-
-
 def welcome_user():
     """Greeting.
 
@@ -25,6 +20,26 @@ def welcome_user():
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
     return name
+
+
+def define_rules():
+    """Rules of the game."""
+    print('What is the result of the expression?')
+
+
+def question(num1, operator, num2):
+    """Question to the player.
+
+    Args:
+        num1: first operand (int)
+        operator: operator (str)
+        num2: second operand (int).
+
+    Returns:
+        Returns answer of the player.
+    """
+    print('Question: {0} {1} {2}'.format(num1, operator, num2))
+    return prompt.string('Your answer: ')
 
 
 def calc(num1, operator, num2):
@@ -45,7 +60,7 @@ def calc(num1, operator, num2):
     return str(operators[operator](num1, num2))
 
 
-def round(name, amount_of_rounds=3):
+def game(name, amount_of_rounds=3):
     """One round of a game.
 
     Args:
@@ -56,18 +71,16 @@ def round(name, amount_of_rounds=3):
         Returns nothing or recursively itself.
     """
     if amount_of_rounds <= 0:
-        print('Congratulations, {0}!'.format(name))
+        return print('Congratulations, {0}!'.format(name))
+    num1, num2 = crypto.randrange(100), crypto.randrange(10)
+    operator = crypto.choice(operators_as_str)
+    answer = question(num1, num2, operator)
+    calculation = calc(num1, operator, num2)
+    if answer == calculation:
+        print('Correct!')
+        return game(name, amount_of_rounds - 1)
     else:
-        num1, num2 = crypto.randrange(100), crypto.randrange(10)
-        operator = crypto.choice(operators_as_str)
-        expression = 'Question: {0} {1} {2}'
-        print(expression.format(num1, operator, num2))
-        answer = prompt.string('Your answer: ')
-        if answer == calc(num1, operator, num2):
-            print('Correct!')
-            return round(name, amount_of_rounds - 1)
-        else:
-            message = ("'{0}' is wrong answer ;(. Correct answer was '{1}'.\n" +
-                       "Let's try again, {2}!"
-                       ).format(answer, calc(num1, operator, num2), name)
-            print(message)
+        message = ("'{0}' is wrong answer ;(. Correct answer was '{1}'.\n" +
+                   "Let's try again, {2}!"
+                   ).format(answer, calculation, name)
+        print(message)
