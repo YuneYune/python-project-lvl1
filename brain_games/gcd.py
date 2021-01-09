9ff6efe857857bb52a3adc8e6d45aeed7a83621c
Dobro <matematikaigor@gmail.com>
2021-01-09 08:42:57 +0300

    refactor gcd game functions

--- brain_games/gcd.py
+++ brain_games/gcd.py
@@ -38,20 +38,34 @@ def find_gcd(num1, num2):
     [less, bigger] = sorted([num1, num2])
     for divisor in range(less, 0, -1):
         if less % divisor == 0 and bigger % divisor == 0:
-            return str(divisor)
+            return divisor
 
 
-def question(num1, num2):
-    """Question to the player.
+def create_task():
+    """Create 2 numbers and find the greatest common divisor of them.
+
+    В такой реализации создания таски шанс того, что НОД != 1 или != 2 немногим
+    меньше , как и НОД = 1 или = 2
+
+    Returns:
+        Returns 2 numbers and the greatest common divisor of them.
+    """
+    main = crypto.randrange(1, 11)  # noqa: WPS432
+    num1 = main * crypto.randrange(1, 10)
+    num2 = main * crypto.randrange(1, 10)
+    return (num1, num2, find_gcd(num1, num2))
+
+
+def question(*args):
+    """Ask the question to the player.
 
     Args:
-        num1: first number (int)
-        num2: second number (int).
+        *args: numbers (int) and/or operator (str).
 
     Returns:
         Returns answer of the player.
     """
-    print('Question: {0} {1}'.format(num1, num2))
+    print('Question: {0} {1}'.format(args[0], args[1]))
     return prompt.string('Your answer: ')
 
 
@@ -67,11 +81,9 @@ def game(name, amount_of_rounds=3):
     """
     if amount_of_rounds <= 0:
         return print('Congratulations, {0}!'.format(name))
-    num1 = crypto.randrange(1, 100)
-    num2 = crypto.randrange(1, 100)
-    answer = question(num1, num2)
-    gcd = find_gcd(num1, num2)
-    if answer == gcd:
+    (first_num, second_num, gcd) = create_task()
+    answer = question(first_num, second_num)
+    if int(answer) == gcd:
         print('Correct!')
         return game(name, amount_of_rounds - 1)
     else:
